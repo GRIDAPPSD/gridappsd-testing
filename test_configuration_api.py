43b2d887356f92cea1e4da1797f7f5e1cfad32d0
khushbooG9 <kgn9294@gmail.com>
2021-04-01 16:05:02 -0700

    Added tests for all files and base files

--- test_configuration_api.py
+++ test_configuration_api.py
@@ -79,49 +79,104 @@ def test_CIM_dictionary_file_output(gridappsd_client, model_name, model_id):
 
     difference = diff(response["data"], result)
 
-    #print(list(difference))
+    if len(list(difference))!=0:
+        LOGGER.info("The data recieved is different as :")
+        print(list(difference))
 
     assert len(list(difference))==0
 
 
-# @pytest.mark.parametrize("model_name, model_id", [
-#     ("ieee123", "_C1C3E687-6FFD-C753-582B-632A27E28507"),
-#     # ("ieee123pv", "_E407CBB6-8C8D-9BC9-589C-AB83FBF0826D"),
-#     # ("ieee123transactive", "_503D6E20-F499-4CC7-8051-971E23D0BF79"),
-#     # ("ieee13nodeckt", "_49AD8E07-3BF9-A4E2-CB8F-C3722F837B62"),
-#     # ("ieee13nodecktassets", "_5B816B93-7A5F-B64C-8460-47C17D6E4B0F"),
-#     # ("test9500new", "_AAE94E4A-2465-6F5E-37B1-3E72183A4E44"),
-# ])
-# def test_base_file_output(gridappsd_client, model_name, model_id):
+@pytest.mark.parametrize("model_name, model_id", [
+    ("ieee123", "_C1C3E687-6FFD-C753-582B-632A27E28507"),
+    ("ieee123pv", "_E407CBB6-8C8D-9BC9-589C-AB83FBF0826D"),
+    ("ieee123transactive", "_503D6E20-F499-4CC7-8051-971E23D0BF79"),
+    ("ieee13nodeckt", "_49AD8E07-3BF9-A4E2-CB8F-C3722F837B62"),
+    ("ieee13nodecktassets", "_5B816B93-7A5F-B64C-8460-47C17D6E4B0F"),
+    ("test9500new", "_AAE94E4A-2465-6F5E-37B1-3E72183A4E44"),
+])
+def test_base_file_output(gridappsd_client, model_name, model_id):
 
-# 	result_file = os.path.join(os.path.dirname(__file__), f"simulation_baseline_files/configuration_api/Gridlabd_base_files/{model_name}.json")
+    result_file = os.path.join(os.path.dirname(__file__), f"simulation_baseline_files/configuration_api/Gridlabd_base_files/{model_name}.json")
 
-# 	gapps = gridappsd_client
+    gapps = gridappsd_client
     
-# 	query = {
-#             "configurationType": "GridLAB-D Base GLM",
-#             "parameters": {
-#                 "i_fraction": "1.0",
-#                 "z_fraction": "0.0",
-#                 "model_id": model_id,
-#                 "load_scaling_factor": "1.0",
-#                 "schedule_name": "ieeezipload",
-#                 "p_fraction": "0.0"
-#                           }
-#             }
+    query = {
+            "configurationType": "GridLAB-D Base GLM",
+            "parameters": {
+                "i_fraction": "1.0",
+                "z_fraction": "0.0",
+                "model_id": model_id,
+                "load_scaling_factor": "1.0",
+                "schedule_name": "ieeezipload",
+                "p_fraction": "0.0"
+                          }
+            }
 
-# 	# response = gapps.get_response(t.CONFIG ,query, timeout=300)
+    response = gapps.get_response(t.CONFIG ,query, timeout=300)
 
-# 	# print(response)
+    #print(response['message'])
 	
-# 	with open(result_file, 'r') as fl:
-# 	   result = json.load(fl)
-#         result = json.loads(result)
-# 	print(result, type(result))
+    with open(result_file, 'r') as fl:
+        result = json.load(fl)
+        
+    #print(result, type(result))
 
-	# difference = diff(response["data"], result)
+    difference = diff(response['message'], result)
 
-	# print(list(difference))
+    if len(list(difference))!=0:
+        LOGGER.info("The data recieved is different as :")
+        print(list(difference))
 
-	# assert len(list(difference))==0
+    assert len(list(difference))==0
+
+@pytest.mark.parametrize("model_name, model_id", [
+    ("ieee123", "_C1C3E687-6FFD-C753-582B-632A27E28507"),
+    ("ieee123pv", "_E407CBB6-8C8D-9BC9-589C-AB83FBF0826D"),
+    ("ieee123transactive", "_503D6E20-F499-4CC7-8051-971E23D0BF79"),
+    ("ieee13nodeckt", "_49AD8E07-3BF9-A4E2-CB8F-C3722F837B62"),
+    ("ieee13nodecktassets", "_5B816B93-7A5F-B64C-8460-47C17D6E4B0F"),
+    ("test9500new", "_AAE94E4A-2465-6F5E-37B1-3E72183A4E44"),
+])
+def test_all_files_output(gridappsd_client, model_name, model_id):
+
+    result_file = os.path.join(os.path.dirname(__file__), f"simulation_baseline_files/configuration_api/Gridlabd_all_files/{model_name}.json")
+
+    gapps = gridappsd_client
+    
+    query = {
+            "configurationType": "GridLAB-D All",
+            "parameters": {
+            "load_scaling_factor": "1.0",
+            "i_fraction": "1.0",
+            "model_id": model_id,
+            "p_fraction": "0.0",
+            "simulation_id": "12345",
+            "z_fraction": "0.0",
+            "simulation_broker_host": "localhost",
+            "simulation_name": "ieee8500",
+            "simulation_duration": "60",
+            "simulation_start_time": "1518958800",
+            "solver_method": "NR",
+            "schedule_name": "ieeezipload",
+            "simulation_broker_port": "61616",
+            "directory": "/tmp/gridlabdsimulation/"
+                          }
+            }
+
+    response = gapps.get_response(t.CONFIG ,query, timeout=300)
+
+    #print(response['message'])
+    
+    with open(result_file, 'r') as fl:
+        result = json.load(fl)
+        
+    #print(result, type(result))
+
+    difference = diff(response['message'], result)
+
+    if len(list(difference))!=0:
+        LOGGER.info("The data recieved is different as :")
+        print(list(difference))
+
+    assert len(list(difference))==0
 	
