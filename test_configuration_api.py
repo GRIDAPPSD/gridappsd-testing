22ba36acd861fe587da8116e4bf57118f5b3c873
khushbooG9 <kgn9294@gmail.com>
2021-04-13 10:12:38 -0700

    added test for CIM feeder index file

--- test_configuration_api.py
+++ test_configuration_api.py
@@ -179,4 +179,42 @@ def test_all_files_output(gridappsd_client, model_name, model_id):
         print(list(difference))
 
     assert len(list(difference))==0
+
+@pytest.mark.parametrize("model_name, model_id", [
+    ("ieee123", "_C1C3E687-6FFD-C753-582B-632A27E28507"),
+    ("ieee123pv", "_E407CBB6-8C8D-9BC9-589C-AB83FBF0826D"),
+    ("ieee123transactive", "_503D6E20-F499-4CC7-8051-971E23D0BF79"),
+    ("ieee13nodeckt", "_49AD8E07-3BF9-A4E2-CB8F-C3722F837B62"),
+    ("ieee13nodecktassets", "_5B816B93-7A5F-B64C-8460-47C17D6E4B0F"),
+    ("test9500new", "_AAE94E4A-2465-6F5E-37B1-3E72183A4E44"),
+])
+def test_CIM_feeder_index_file_output(gridappsd_client, model_name, model_id):
+
+    result_file = os.path.join(os.path.dirname(__file__), f"simulation_baseline_files/configuration_api/Gridlabd_all_files/{model_name}.json")
+
+    gapps = gridappsd_client
+    
+    query = {
+            "configurationType":"CIM Feeder Index",
+            "parameters": {
+                "model_id":model_id
+                           }
+            }
+
+    response = gapps.get_response(t.CONFIG ,query, timeout=300)
+
+    #print(response)
+    
+    with open(result_file, 'r') as fl:
+        result = json.load(fl)
+        
+    #print(result, type(result))
+
+    difference = diff(response['data'], result)
+
+    if len(list(difference))!=0:
+        LOGGER.info("The data recieved is different as :")
+        print(list(difference))
+
+    assert len(list(difference))==0
 	
